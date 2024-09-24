4a636f5c8e9aaf7b86d4831211808b9e5d62bf94
Luis Manuel Brito <lmbritor@pm.com>
2024-09-24 09:16:51 -0400

    edit: vuelvo a borrar el ts error

--- src/modules/products/components/product-tabs/accordion.tsx
+++ src/modules/products/components/product-tabs/accordion.tsx
@@ -54,13 +54,14 @@ const Item: React.FC<AccordionItemProps> = ({
         className
       )}
     >
+
             <AccordionPrimitive.Header className="px-1">
         <div className="flex flex-col">
           <div className="flex w-full items-center justify-between">
             <div className="flex items-center gap-4">
               <Text className="text-ui-fg-subtle text-sm">{title}</Text>
             </div>
-            
+
             <AccordionPrimitive.Trigger>
               {customTrigger || <MorphingTrigger />}
             </AccordionPrimitive.Trigger>
